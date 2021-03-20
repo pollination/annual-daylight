3a8303ac33234f591c7e2d20e3f2f1a907363bde
Chris Mackey <chris@ladybug.tools>
2021-03-19 22:28:03 -0400

    fix(alias): Update aliases

--- pollination/annual_daylight/entry.py
+++ pollination/annual_daylight/entry.py
@@ -11,10 +11,13 @@ from pollination.honeybee_radiance.post_process import AnnualDaylightMetrics
 from pollination.alias.inputs.model import hbjson_model_input
 from pollination.alias.inputs.wea import wea_input
 from pollination.alias.inputs.north import north_input
+from pollination.alias.inputs.radiancepar import rad_par_annual_input, \
+    daylight_thresholds_input
+from pollination.alias.inputs.grid import sensor_count_input, grid_filter_input
 from pollination.alias.inputs.schedule import schedule_csv_input
-from pollination.alias.outputs.daylight import sort_annual_daylight_results, \
+from pollination.alias.outputs.daylight import annual_daylight_results, \
     daylight_autonomy_results, continuous_daylight_autonomy_results, \
-    useful_daylight_illuminance_results
+    udi_results, udi_lower_results, udi_upper_results
 
 
 from ._raytracing import AnnualDaylightRayTracing
@@ -35,17 +38,23 @@ class AnnualDaylightEntryPoint(DAG):
     sensor_count = Inputs.int(
         default=200,
         description='The maximum number of grid points per parallel execution.',
-        spec={'type': 'integer', 'minimum': 1}
+        spec={'type': 'integer', 'minimum': 1},
+        alias=sensor_count_input
     )
 
     radiance_parameters = Inputs.str(
         description='The radiance parameters for ray tracing.',
-        default='-ab 2 -ad 5000 -lw 2e-05'
+        default='-ab 2 -ad 5000 -lw 2e-05',
+        alias=rad_par_annual_input
     )
 
-    sensor_grid = Inputs.str(
-        description='A grid name or a pattern to filter the sensor grids. By default '
-        'all the grids in HBJSON model will be exported.', default='*'
+    grid_filter = Inputs.str(
+        description='Text for a grid identifer or a pattern to filter the sensor grids '
+        'of the model that are simulated. For instance, first_floor_* will simulate '
+        'only the sensor grids that have an identifier that starts with '
+        'first_floor_. By default, all grids in the model will be simulated.',
+        default='*',
+        alias=grid_filter_input
     )
 
     model = Inputs.file(
@@ -73,7 +82,8 @@ class AnnualDaylightEntryPoint(DAG):
         'threshold. The default is -t 300 -lt 100 -ut 3000. The order of the keys is not '
         'important and you can include one or all of them. For instance if you only '
         'want to change the upper threshold to 2000 lux you should use -ut 2000 as '
-        'the input.', default='-t 300 -lt 100 -ut 3000'
+        'the input.', default='-t 300 -lt 100 -ut 3000',
+        alias=daylight_thresholds_input
     )
 
     @task(template=CreateSunMatrix)
@@ -88,7 +98,7 @@ class AnnualDaylightEntryPoint(DAG):
         ]
 
     @task(template=CreateRadianceFolder)
-    def create_rad_folder(self, input_model=model, sensor_grid=sensor_grid):
+    def create_rad_folder(self, input_model=model, sensor_grid=grid_filter):
         """Translate the input model to a radiance folder."""
         return [
             {'from': CreateRadianceFolder()._outputs.model_folder, 'to': 'model'},
@@ -202,7 +212,7 @@ class AnnualDaylightEntryPoint(DAG):
 
     results = Outputs.folder(
         source='results', description='Folder with raw result files (.ill) that '
-        'contain illuminance matrices.', alias=sort_annual_daylight_results
+        'contain illuminance matrices.', alias=annual_daylight_results
     )
 
     metrics = Outputs.folder(
@@ -221,17 +231,17 @@ class AnnualDaylightEntryPoint(DAG):
 
     udi = Outputs.folder(
         source='metrics/udi', description='Useful daylight illuminance results.',
-        alias=useful_daylight_illuminance_results
+        alias=udi_results
     )
 
     udi_lower = Outputs.folder(
         source='metrics/udi_lower', description='Results for the percent of time that '
         'is below the lower threshold of useful daylight illuminance.',
-        alias=useful_daylight_illuminance_results
+        alias=udi_lower_results
     )
 
     udi_upper = Outputs.folder(
         source='metrics/udi_upper', description='Results for the percent of time that '
         'is above the upper threshold of useful daylight illuminance.',
-        alias=useful_daylight_illuminance_results
+        alias=udi_upper_results
     )
