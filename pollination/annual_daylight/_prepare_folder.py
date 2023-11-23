fe2a244112acd97876e90a5221e67c98f31f2046
Mikkel Pedersen <mkklpdrsn@gmail.com>
2023-11-23 12:52:01 +0100

    fix(timestep): Relax restrictions on wea and add timestep

--- pollination/annual_daylight/_prepare_folder.py
+++ pollination/annual_daylight/_prepare_folder.py
@@ -6,10 +6,11 @@ from pollination.honeybee_radiance.translate import CreateRadianceFolderGrid
 from pollination.honeybee_radiance.sky import CreateSkyDome, CreateSkyMatrix
 from pollination.honeybee_radiance.octree import CreateOctreeStatic
 from pollination.honeybee_radiance.grid import SplitGridFolder
+from pollination.honeybee_radiance.study import StudyInfo
 
 # input/output alias
 from pollination.alias.inputs.model import hbjson_model_grid_input
-from pollination.alias.inputs.wea import wea_input_timestep_check
+from pollination.alias.inputs.wea import wea_input
 from pollination.alias.inputs.north import north_input
 from pollination.alias.inputs.grid import grid_filter_input, \
     min_sensor_count_input, cpu_count
@@ -66,10 +67,16 @@ class AnnualDaylightPrepareFolder(GroupedDAG):
     wea = Inputs.file(
         description='Wea file.',
         extensions=['wea', 'epw'],
-        alias=wea_input_timestep_check
+        alias=wea_input
     )
 
-    @task(template=CreateSunMtx)
+    timestep = Inputs.int(
+        description='Input wea timestep. This value will be used to compute '
+        'cumulative radiation results.', default=1,
+        spec={'type': 'integer', 'minimum': 1, 'maximum': 60}
+    )
+
+    @task(template=CreateSunMatrix)
     def generate_sunpath(self, north=north, wea=wea):
         """Create sunpath for sun-up-hours."""
         return [
@@ -153,6 +160,15 @@ class AnnualDaylightPrepareFolder(GroupedDAG):
             {
                 'from': ParseSunUpHours()._outputs.sun_up_hours,
                 'to': 'results/sun-up-hours.txt'
+            }
+        ]
+
+    @task(template=StudyInfo)
+    def create_study_info(self, wea=wea, timestep=timestep):
+        return [
+            {
+                'from': StudyInfo()._outputs.study_info,
+                'to': 'results/study_info.json'
             }
         ]
 
